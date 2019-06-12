241cad33cd4a62122f175019dfdc443a46e5981a
Aiden Tai <aiden.tai@c3.ai>
2019-06-12 11:32:13 -0700

    Cleaning up temporary files

--- c3edEnv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/baljeetyadav/Downloads/c3EngineeringDashboard/c3edEnv/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
