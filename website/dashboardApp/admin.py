d8807d807cf15b09f220c8e5dbd74363d836a763
Aiden Tai <aiden.tai@c3.ai>
2019-06-21 11:18:54 -0700

    Added UserProfile to admin.py

--- website/dashboardApp/admin.py
+++ website/dashboardApp/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import JiraStatistics, GithubPullRequestSize, GithubPullRequestNotification
+from .models import UserProfile, JiraStatistics, GithubPullRequestSize, GithubPullRequestNotification
 
 
+admin.site.register(UserProfile)
 admin.site.register(JiraStatistics)
 admin.site.register(GithubPullRequestSize)
 admin.site.register(GithubPullRequestNotification)
